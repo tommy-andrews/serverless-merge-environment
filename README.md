e10b6c22b05b0910d4c1f6fc7415c74a25ec75c9
Tommy Andrews <tommy.andrews@ssense.com>
2023-01-13 11:59:11 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Serverless Merge Environment
+
+Based on [serverless-merge-config](https://github.com/CruGlobal/serverless-merge-config)
+
+## Installation
+
+```
+npm install --save-dev @tommy-andrews/serverless-merge-environment
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const {
+    assign,
+    isArray,
+    isPlainObject
+} = require('lodash');
+
+class ServerlessMergeEnvironment {
+    constructor (serverless) {
+        this.serverless = serverless;
+        serverless.configSchemaHandler.defineCustomProperties({
+            type: 'object',
+            properties: {
+                mergeEnvironment: {
+                    type: 'array'
+                },
+            },
+            required: ['mergeEnvironment']
+        });
+
+        this.hooks = {
+            'before:package:initialize': this.mergeConfig.bind(this),
+            'before:offline:start:init': this.mergeConfig.bind(this),
+            'before:invoke:local:invoke': this.mergeConfig.bind(this)
+        };
+    }
+
+    mergeConfig () {
+        if (isArray(this.serverless.service.custom.mergeEnvironment)) {
+            this.serverless.service.custom.mergeEnvironment.forEach((subValue) => {
+                this.assignValue(this.serverless.service.provider.environment, subValue);
+            })
+        } else {
+            this.assignValue(this.serverless.service.provider.environment, value);
+        }
+    }
+
+    assignValue (collection, value) {
+        if (isPlainObject(value)) {
+            // Only merge objects
+            assign(collection, value);
+        }
+    }
+}
+
+module.exports = ServerlessMergeEnvironment;
